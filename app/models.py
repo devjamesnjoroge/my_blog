d8c112110a12d1b55d23a98bb8181eccf1b8531e
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-05-14 20:36:55 +0300

    ft: create models user class

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from . import db
+
 class Quote:
 
     def __init__(self, author, id, quote, permalink):
@@ -7,6 +9,14 @@ class Quote:
         self.quote = quote
         self.permalink = permalink
 
-    
+class User(db.Model):
 
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'{self.username}'
 
