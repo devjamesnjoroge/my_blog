80e4516749f077ad359d122cf843f0d7f500ae4e
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-05-14 20:27:56 +0300

    ft: add quote get method

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,11 @@
 from flask import render_template
 from . import main
+from ..requests import get_Quote
 
 @main.route('/')
 def index():
+
+    quote = get_Quote()
+
     title = 'JAYMMY-TECH'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title, quote = quote)
--- app/requests.py
+++ app/requests.py
@@ -1,4 +1,5 @@
 import urllib.request, json
+from app.models import Quote
 
 def get_Quote():
 
@@ -6,3 +7,15 @@ def get_Quote():
 
     with urllib.request.urlopen(base_url) as url:
         get_quote_data = json.loads(url.read())
+
+        get_quote_results = None
+
+        if get_quote_data:
+            author = get_quote_data['author']
+            id = get_quote_data['id']
+            quote = get_quote_data['quote']
+            permalink = get_quote_data['permalink']
+
+            get_quote_results = Quote(author, id, quote, permalink)
+
+    return get_quote_results
