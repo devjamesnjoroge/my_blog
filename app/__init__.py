c56e2488f545c886b6171da5f7a10541caeb2d20
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-05-14 16:38:21 +0300

    ft: register blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -9,4 +9,12 @@ def create_app(config_name):
 
     app.config.from_object(config_options[config_name])
 
+    # Register Blueprints
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, urlprefix = 'auth')
+
     return app
