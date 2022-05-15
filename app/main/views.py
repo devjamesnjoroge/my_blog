19552f611a28d5a36632eeef8bfd188e735e1780
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-05-15 18:21:36 +0300

    ft: add update profile template

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,9 @@ from flask import render_template,request,redirect,url_for,abort
 from . import main
 from ..models import User
 from ..requests import get_Quote
+from .. import db
+from .forms import UpdateProfile
+from flask_login import login_required
 
 @main.route('/')
 def index():
@@ -19,3 +22,21 @@ def profile(uname):
         abort(404)
 
     return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        user.save_user()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
