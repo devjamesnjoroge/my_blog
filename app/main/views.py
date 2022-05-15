29f0ba7794b3a1d233292b7253fdbe7c0afe1fbf
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-05-15 18:12:33 +0300

    ft: add profile page

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for,abort
 from . import main
+from ..models import User
 from ..requests import get_Quote
 
 @main.route('/')
@@ -9,3 +10,12 @@ def index():
 
     title = 'JAYMMY-TECH'
     return render_template('index.html', title = title, quote = quote)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
